c4de9377676767180d5efc534e01e6e02a8666fa
Andier Prattes Ramos <andier@agafarma.com.br>
2025-08-28 21:33:31 -0300

    commit - scriptMysql

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,5 +1,7 @@
 using BancoDigitalAPI.Models;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Net;
 
 namespace BancoDigitalAPI.Data;
 
@@ -7,5 +9,16 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     public DbSet<ContaCorrente> Contas => Set<ContaCorrente>();
+
+
+   // Método OnModelCreating no DbContext para configurar o tamanho e tipo dos campos da tabela no banco de dados.
+   // Usando Fluent API do Entity Framework Core.
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ContaCorrente>()
+            .Property(c => c.Saldo)
+            .HasPrecision(14, 2); // 14 dígitos no total e 2 decimais
+    }
 }
 
+ 
